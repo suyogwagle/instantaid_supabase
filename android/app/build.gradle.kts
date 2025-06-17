48eeac32b090bb0efabb56f8df1753fd3619f265
Rabinn1011 <rabin.lc99@gmail.com>
2025-06-17 08:39:19 +0545

    Initial commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.google.gms.google-services")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.instant_aid"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.instant_aid"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+    implementation("com.google.firebase:firebase-auth")
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/example/instant_aid/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.instant_aid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
